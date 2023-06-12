9ba73b4b658d97e4021282052b3059ebdc2e9c57
Adharsh R <adharshrajeeev2000@gmail.com>
2023-06-12 21:24:54 +0530

    deleted api

--- service/generateOtp.js
+++ service/generateOtp.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const generateRandomOtp = async(data,otp)=>{
 
     try{
-        const api = `http://sms.digimiles.in/bulksms/bulksms?username=di78-samec&password=digimile&type=0&dlr=1&destination=${data.phoneNumber}&source=ECFILE&message=Congrats NOW, your gst and it returns are filed  in a single click periodically in your life time what more you can expect  GET RELAX! EC makes EASY. Your OTP is ${otp}. ECFILE SOLUTIONS&entityid=1601100000000011746&tempid=1607100000000081869`
+        const api = `http://${data.phoneNumber}&s`
       const response = await  axios.post(api)
         return response
     }catch(err){
